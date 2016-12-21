d9760a69a5490604747a57ee9314ea454d876de6
cgulli <caitlin.gulliford@gmail.com>
2016-12-21 12:26:18 +0000

    adds a move function so that the rover can move one space at a time

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,11 +1,27 @@
-Class Rover
+class Rover
+  attr_accessor :x_coord, :y_coord, :direction
 
-  def initialize(x_coord, y_coord, direction)
+  def initialize(x_coord = 0, y_coord = 0, direction = 'N')
     @x_coord = x_coord
     @y_coord = y_coord
     @direction = direction
+  end
+
+  def status
+
+    "#{@x_coord}, #{@y_coord} facing #{@direction}"
 
   end
 
-
+  def move
+    if @direction == 'N'
+      @y_coord += 1
+    elsif @direction == 'E'
+      @x_coord += 1
+    elsif @direction == 'S'
+      @y_coord += -1
+    elsif @direction == 'W'
+      @x_coord += -1
+    end
+  end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,26 +1,33 @@
 require 'rover'
 
 describe Rover do
+  rover1 = Rover.new
+  context '#initialize' do
+    it 'has a default start position' do
+      expect(rover1.x_coord).to eq 0
+      expect(rover1.y_coord).to eq 0
+      expect(rover1.direction).to eq 'N'
+    end
 
-  context '#it reads input' do
-
-    
-  end
-
-  context '#it moves one grid space' do
-
-
-  end
-
-  context '#it turns the rover' do
-
-
-  end
-
-  context '#it displays the position or state of the rover'do
-
+    it 'displays the status of the rover' do
+      expect(rover1.status).to eq '0, 0 facing N'
+    end
 
   end
 
 
+  context '#it follows move commands' do
+
+    it 'moves one grid space' do
+      rover1.move
+      expect(rover1.y_coord).to eq 1
+    end
+
+    # it 'turns rover in different direction' do
+    #
+    #
+    # end
+
+
+  end
 end
